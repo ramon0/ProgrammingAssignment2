e3a5fc6efd7ac888bed85c76001ea7db57ed12a2
ramon0 <rpujolp@gmail.com>
2015-07-17 18:59:02 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,17 @@
+#This function is the constructor of matrix object and their functions associted
 makeVector <- function(x = numeric()) {
+  #m is the variable cached inicialized with NULL
   m <- NULL
+  #function that set the free variable x
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
+  #get the free variable
   get <- function() x
+  #cache the mean
   setmean <- function(mean) m <<- mean
+  #get the mean
   getmean <- function() m
   list(set = set, get = get,
        setmean = setmean,
@@ -13,11 +19,14 @@ makeVector <- function(x = numeric()) {
 }
 
 cachemean <- function(x, ...) {
-  m <- x$getmean()
+  #get the mean
+  m <- x$getmean()  	
   if(!is.null(m)) {
+    #if exist the mean in cache get her and return
     message("getting cached data")
     return(m)
   }
+  #if doesn't exist calculate and cache her
   data <- x$get()
   m <- mean(data, ...)
   x$setmean(m)
